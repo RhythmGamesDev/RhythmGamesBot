1f7ec385a20219c7b877f0a5700943ed4750c970
HTG-YT <htgazurex1212.0001@gmail.com>
2021-05-03 21:06:31 +0800

    Add Option<T> for possible replacement for null

---
+++ Libraries/RhythmGamesBot.Utils/Option.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace RhythmGamesBot.Utils
+{
+    public struct Option<T>
+    {
+        private bool _isSome;
+        private T _value;
+
+        private Option() 
+        {
+            this._isSome = false;
+        }
+
+        private Option(T value)
+        {
+            this._value = value;
+            this._isSome = true;
+        }
+
+        public static Option Some(T value)
+        {
+            return new Option(value);
+        }
+
+        public static Option None() 
+        {
+            return new Option();
+        }
+
+        public bool IsSome() 
+        {
+            return this._isSome;
+        }
+
+        public bool IsNone()
+        {
+            return !this._isSome;
+        }
+
+        public T Unwrap() 
+        {
+            return this._isSome switch 
+            {
+                true => this._value,
+                _ => throw new InvalidOperationException("Attempted to call `.Unwrap()` on an `Option.None()` value.")
+            };
+        }
+
+        public Option<T> Replace(T value)
+        {
+            if (this._isSome) 
+            {
+                T temp = this._value;
+                this._value = value;
+
+                return temp;
+            }
+
+            this._value = value;
+            return Option<T>.None();
+        }
+    }
+}
