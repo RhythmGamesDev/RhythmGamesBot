b5dc5260f54067798ce7d40905d3fa9bd280eb9c
The1Moxie <81781818+The1Moxie@users.noreply.github.com>
2021-05-04 20:27:08 +0100

    adding bot variables
    added the config variables to make the bot connect as well as added a class to maintain non public information.
    
    added logging support

--- Libraries/RhythmGamesBot.Core/RhythmGamesBot.cs
+++ Libraries/RhythmGamesBot.Core/RhythmGamesBot.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 using DSharpPlus;
-
+using Microsoft.Extensions.Logging;
 using RhythmGamesBot.Utils;
 
 namespace RhythmGamesBot.Core
@@ -17,6 +17,18 @@ namespace RhythmGamesBot.Core
             {
                 // We need to put fields here if we were to use the default configuration (aka the one used for actual production use),
                 // the reasoning behind the Optional configuration is that we can tweak the configuration for testing purposes.
+
+                //Bot Configurations
+                Token = new BotSecrets().token, //class containing the token, you can impliment your own security system or data management.
+                TokenType = TokenType.Bot,
+
+                //Since the bot is going to use discord events in need to be able to use all intents.
+                Intents = DiscordIntents.All,
+
+                //logging
+                MinimumLogLevel = LogLevel.Debug,
+                LogTimestampFormat = "MMM dd yyyy - hh:mm:ss tt" //time on log set as example Jan 01 2021 - 07:07:07 PM
+
             };
 
             BotInstance = Option<DiscordClient>.Some(new DiscordClient(configuration));
